eafae22d1b1c41cf6c25bbe5bd37be448c0eb614
SnikS1337 <35911914+SnikS1337@users.noreply.github.com>
2026-03-25 11:33:22 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Qa-Trainer/',
+    base: '/1qa-gemini_copy/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
