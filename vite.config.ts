18a83656435cbaac19d3eca29feddaecafe33326
nixxi <nixxi00@mail.ru>
2026-03-25 11:38:06 +0300

    fix: add base path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/1qa-gemini_copy/',
+    base: '/Qa-Trainer/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
